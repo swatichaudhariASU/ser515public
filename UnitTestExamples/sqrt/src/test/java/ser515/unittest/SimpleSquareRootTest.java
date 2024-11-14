cacde4987c200cc83ab011a071a702c60b61fba2
swatichaudhariASU <suchaudh@asu.edu>
2024-11-14 14:56:38 -0700

    task2

--- UnitTestExamples/sqrt/src/test/java/ser515/unittest/SimpleSquareRootTest.java
+++ UnitTestExamples/sqrt/src/test/java/ser515/unittest/SimpleSquareRootTest.java
@@ -3,6 +3,7 @@ package ser515.unittest;
 // static imports are just a scoping mechanism for static members of an imported class
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Disabled;
 
@@ -22,7 +23,23 @@ class SimpleSquareRootTest {
     void testzerosquareMe() {
 	assertEquals(0, SimpleSquareRootFunction.squareMe(0));
     
-}
+    }
+
+    @Test
+    void task2squareMe() {
+            Assertions.assertAll(
+            () -> assertTrue(makeSqrt(0)),
+            () -> assertFalse(makeSqrt(1))
+    );
+    // assertEquals(0, SimpleSquareRootFunction.makeSqrt(0));
+    }
+
+    @Test
+    void task2_2squareMe() {
+        Assertions.assertTrue(makeSqrt(0));
+        Assertions.assertFalse(makeSqrt(1));
+    // assertEquals(0, SimpleSquareRootFunction.makeSqrt(0));
+    }
 
     //@Disabled("Don't want to run this just yet")
     //@Test
